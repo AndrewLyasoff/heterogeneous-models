5df172abf1bcc3efeb05fcbd027a1d5fdf1e7bcc
Andrew Lyasoff <andrew.lyasoff@gmail.com>
2023-01-02 14:05:21 -0500

    Add files via upload

--- DIW-0/post-run-DIW-0.jl
+++ DIW-0/post-run-DIW-0.jl
@@ -30,6 +30,9 @@ begin
     using Optim
     include("functions-DIW-0.jl");
     include("ini-setup-RMT-ch18.jl");
+    using FastGaussQuadrature
+    gqx, gqw = gausslegendre( 100 );
+    GQX, GQW = gausslegendre( 10_000 );
 end;
 
 # this block is superfluous if run-DIW-0.jl
@@ -71,7 +74,7 @@ clearing
 #returns -1.7387766322006504e-6
 
 #restore the grid
-gridc=make_grid(g_step,gsz);
+#gridc=make_grid(g_step,gsz);
 
 # plot the distribution of all households over the range of consumption
 # provides the left plot on Figure 5
@@ -79,18 +82,18 @@ begin
     plotgrid=0.0:.001:0.5;
     pval=[F[1](x) for x in plotgrid];
     @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
-     for i=2:length(F)
+    for i=2:length(F)
         local ppval
         ppval=[F[i](x) for x in plotgrid];
         @gp :- plotgrid ppval "w l t '' lw 2.5 lt rgb 'black'";
-     end
+    end
     @gp :- "set auto fix";
 end
 
 # same distribution as a density
 # provides the right plot on Figure 5
 begin
-    plotgrid=gridc[1]:.001:0.5;
+    plotgrid=cgrid[1]:.001:0.5;
     pval=[Interpolations.gradient(F_spline[1],x)[1] for x in plotgrid];
     @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
     for i=2:length(F)
@@ -101,19 +104,96 @@ begin
     @gp :- "set auto fix";
 end
 
+## STATISTICS
+
+begin
+    cstep=(cgrid[end]-cgrid[1])*(1/20000);
+    gridac=[cgrid[1]+i*cstep for i=0:20000];
+    dF=[(F[i].(gridac[2:end])-F[i].(gridac[1:end-1])) for i=1:nos];
+    gridacm=(gridac[1:end-1]+gridac[2:end])*(1/2);
+    EE=[dot(gridacm,dF[i]) for i=1:nos];
+    CM2=[dot((gridacm.-EE[i]).^2,dF[i]) for i=1:nos];
+    CM3=[dot((gridacm.-EE[i]).^3,dF[i]) for i=1:nos];
+    CM4=[dot((gridacm.-EE[i]).^4,dF[i]) for i=1:nos];
+end;
+
+# consumption avergaes across the employment categories
+(x->round(x,digits=5)).(EE)
+#=
+ 0.2044
+ 0.208
+ 0.21224
+ 0.21738
+ 0.2238
+ 0.23206
+ 0.24307
+=#
+
+# standard deviation
+(x->round(x,digits=5)).(sqrt.(CM2))
+#=
+7-element Vector{Float64}:
+ 0.0458
+ 0.04422
+ 0.04271
+ 0.04132
+ 0.04011
+ 0.03909
+ 0.03827
+=#
+
+#skewness
+(x->round(x,digits=5)).([CM3[i]/(CM2[i]^(3/2)) for i=1:nos])
+#=
+7-element Vector{Float64}:
+ 0.11558
+ 0.27624
+ 0.43391
+ 0.57253
+ 0.68702
+ 0.77908
+ 0.84976
+=#
+
+#kurtosis
+(x->round(x,digits=5)).([CM4[i]/(CM2[i]^2) for i=1:nos])
+#=
+7-element Vector{Float64}:
+ 3.86671
+ 3.71633
+ 3.63201
+ 3.63853
+ 3.71073
+ 3.81284
+ 3.91739
+=#
+
+## DISTRIBUTION OVER WEALTH
+#
+# invert c ↝ θᵤ(c)*SPOT so that c ↝ Fᵤ(c) can be expressed θᵤ*SPOT ↝ Fᵤ(θᵤ*SPOT)
+#
+begin
+    ran_w_bounds=[(θ[i](0.0)*SPOT,θ[i](cgrid[end])*SPOT) for i=1:nos];
+    ran_w_steps=[(ran_w_bounds[i][2]-ran_w_bounds[i][1])*(1/2000) for i=1:nos];
+    ran_w_range=[(ran_w_bounds[i][1]:ran_w_steps[i]:(ran_w_bounds[i][1]+2000*ran_w_steps[i])) for i=1:nos];
+    gcmid=(cgrid[1]+cgrid[end])*(1/2);
+    ran_w_vals=[[find_zero(x->θ[i](x)*SPOT-absc,gcmid,Order2()) for absc in ran_w_range[i]] for i=1:nos];
+    FW_spline=[CubicSplineInterpolation(ran_w_range[i],F[i].(ran_w_vals[i]),extrapolation_bc = Interpolations.Line()) for i=1:nos];
+    FW=[x->(if x<ran_w_range[i][1] 0.0 elseif x>ran_w_range[i][end] 1.0 else FW_spline[i](x) end) for i=1:nos];
+end;
+
+minimum([ran_w_range[i][1] for i=1:nos])
 
 # plot the distribution of all households over the range of asset holdings (wealth)
 # provides the left plot on Figure 6
 begin
-    plotgrid=gridc[1]:.001:0.43;
-    pval=[F[1](x) for x in plotgrid];
-    xval=[θ[1](x)*SPOT for x in plotgrid];
-    @gp xval pval "w l t '' lw 2.5 lt rgb 'black'";
-    for i=2:length(F)
-        local ppval, xxval
-        ppval=[F[i](x) for x in plotgrid];
-        xxval=[θ[i](x)*SPOT for x in plotgrid];
-        @gp :- xxval ppval "w l t '' lw 2.5 lt rgb 'black'";
+    plotgrid=(minimum([ran_w_range[i][1] for i=1:nos])-0.1):.01:5.0;
+    pval=[FW[1](x) for x in plotgrid];
+    @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
+    for i=2:length(FW)
+        local ppval
+        ppval=[FW[i](x) for x in plotgrid];
+        @gp :- plotgrid ppval "w l t '' lw 2.5 lt rgb 'black'";
     end
     @gp :- "set auto fix";
 end
@@ -122,24 +202,91 @@ end
 # same distribution as above in the form of density
 # provides the right plot on Figure 6
 begin
-    plotgrid=gridc[1]:.001:0.43;
-    pval=[Interpolations.gradient(F_spline[1],x)[1] for x in plotgrid];
-    xval=[θ[1](x)*SPOT for x in plotgrid];
-    @gp xval pval "w l t '' lw 2.5 lt rgb 'black'";
-    for i=2:length(F)
-        local ppval, xxval
-        ppval=[Interpolations.gradient(F_spline[i],x)[1] for x in plotgrid];
-        xxval=[θ[i](x)*SPOT for x in plotgrid];
-        @gp :- xxval ppval "w l t '' lw 2.5 lt rgb 'black'";
+    plotgrid=(ran_w_range[1][1]+0.06):.005:5.0;
+    pval=[Interpolations.gradient(FW_spline[1],x)[1] for x in plotgrid];
+    @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
+    for i=2:length(FW)
+        local pplotgrid, ppval
+        pplotgrid=(ran_w_range[i][1]+0.06):.005:5.0;
+        ppval=[Interpolations.gradient(FW_spline[i],x)[1] for x in pplotgrid];
+        @gp :- pplotgrid ppval "w l t '' lw 2.5 lt rgb 'black'";
     end
     @gp :- "set auto fix";
 end
 
 
+
+## STATISTICS
+
+begin
+    w_min=minimum([ran_w_range[i][1] for i=1:nos])-0.1
+    wstep=(5.0-w_min)*(1/20000);
+    gridaw=[w_min+i*wstep for i=0:20000];
+    dFW=[(FW[i].(gridaw[2:end])-FW[i].(gridaw[1:end-1])) for i=1:nos];
+    gridawm=(gridaw[1:end-1]+gridaw[2:end])*(1/2);
+    WEE=[dot(gridawm,dFW[i]) for i=1:nos];
+    WCM2=[dot((gridawm.-WEE[i]).^2,dFW[i]) for i=1:nos];
+    WCM3=[dot((gridawm.-WEE[i]).^3,dFW[i]) for i=1:nos];
+    WCM4=[dot((gridawm.-WEE[i]).^4,dFW[i]) for i=1:nos];
+end;
+
+# wealth avergaes across the employment categories
+(x->round(x,digits=5)).(WEE)
+#=
+7-element Vector{Float64}:
+ -0.20375
+ -0.15901
+ -0.09993
+ -0.01888
+  0.09461
+  0.25604
+  0.48922
+=#
+
+# standard deviation
+(x->round(x,digits=5)).(sqrt.(WCM2))
+#=
+7-element Vector{Float64}:
+ 0.92818
+ 0.93036
+ 0.93253
+ 0.93465
+ 0.93657
+ 0.93805
+ 0.93861
+=#
+
+#skewness
+(x->round(x,digits=5)).([WCM3[i]/(WCM2[i]^(3/2)) for i=1:nos])
+#=
+7-element Vector{Float64}:
+ 1.00653
+ 0.99688
+ 0.98671
+ 0.97588
+ 0.96403
+ 0.95015
+ 0.9316
+=#
+
+#kurtosis
+(x->round(x,digits=5)).([WCM4[i]/(WCM2[i]^2) for i=1:nos])
+#=
+7-element Vector{Float64}:
+ 4.14594
+ 4.12455
+ 4.10058
+ 4.07227
+ 4.0367
+ 3.98815
+ 3.91411
+=#
+
+
 # investment as a function of consumption in all 7 states
 # provides the left plot on Figure 7
 begin
-    plotgrid=gridc[1]:.001:gridc[end];
+    plotgrid=cgrid[1]:.001:cgrid[end];
     pval=[θ[1](x)*SPOT for x in plotgrid];
     @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
     for i=2:length(F)
@@ -154,7 +301,8 @@ end
 # investment as a function of consumption in all 7 states near 0
 # provides the right plot on Figure 7
 begin
-    plotgrid=gridc[1]:.0005:0.2;
+    #plotgrid=cgrid[1]:.0005:0.2;
+    plotgrid=0.0:.0005:0.2;
     pval=[θ[1](x)*SPOT for x in plotgrid];
     @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
     for i=2:length(F)
@@ -195,7 +343,7 @@ end
 # future consumption in all 7 states as a function of present consumption in (present) state 2
 # provides the left plot in Figure 8
 begin
-    plotgrid=gridc[1]:.001:gridc[end];
+    plotgrid=cgrid[1]:.001:cgrid[end];
     pval=[cnext[2,1](x) for x in plotgrid];
     @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
     for i=2:length(F)
@@ -212,7 +360,7 @@ end
 # future consumption in all 7 states as a function of present consumption in (present) state 7
 # provides the right plot in Figure 8
 begin
-    plotgrid=gridc[1]:.001:gridc[end];
+    plotgrid=cgrid[1]:.001:cgrid[end];
     pval=[cnext[7,1](x) for x in plotgrid];
     @gp plotgrid pval "w l t '' lw 2.5 lt rgb 'black'";
     for i=2:length(F)
@@ -227,15 +375,15 @@ end
 
 
 ## compute the endogenous bounds for the wealth at grid
-(minimum([θ[k](gridc[1]) for k=1:nos])*SPOT,maximum([θ[k](gridc[end]) for k=1:nos])*SPOT)
+(minimum([θ[k](cgrid[1]) for k=1:nos])*SPOT,maximum([θ[k](cgrid[end]) for k=1:nos])*SPOT)
 # returns: (-1.6274869069693472, 17.937506971712203)
 
 # endogenous borrowing limit for shares held at grid
-minimum([θ[k](gridc[1]) for k=1:nos])
+minimum([θ[k](cgrid[1]) for k=1:nos])
 # returns: -7.738767301581213
 
 ## check the distribution support at grid
-(maximum([F[k](gridc[1]) for k=1:nos]) ,minimum([F[k](gridc[end]) for k=1:nos]))
+(maximum([F[k](cgrid[1]) for k=1:nos]) ,minimum([F[k](cgrid[end]) for k=1:nos]))
 # returns: (5.0147289107981385e-6, 0.9999999999999976)
 
 ## choosing a narrower interval where most of the distribution is amassed
@@ -267,7 +415,7 @@ true_r=ι/SPOT-1.0
 # returns: -1.6272627182032762
 
 # endogenous borrwoing limit for each employment class from DIW
-([θ[k](gridc[1]) for k=1:nos])*SPOT
+([θ[k](cgrid[1]) for k=1:nos])*SPOT
 #=
 7-element Vector{Float64}:
  -1.6257844994637685
@@ -328,13 +476,13 @@ end;
 # provides the first part of Figure 4
 begin
     @gp aassetst[1,2:10:1300] LFt[1][2:10:1300] "w p lt rgb 'black' pt 7 ps 0.75 t ''";
-    @gp :- "set auto fix";
     @gp :- aassetst[2,2:10:1300] LFt[2][2:10:1300] "w p lt rgb 'black' pt 7 ps 0.75 t ''";
     @gp :- aassetst[3,2:10:1300] LFt[3][2:10:1300] "w p lt rgb 'black' pt 7 ps 0.75 t ''";
     @gp :- aassetst[4,2:10:1300] LFt[4][2:10:1300] "w p lt rgb 'black' pt 7 ps 0.75 t ''";
     @gp :- aassetst[5,2:10:1300] LFt[5][2:10:1300] "w p lt rgb 'black' pt 7 ps 0.75 t ''";
     @gp :- aassetst[6,2:10:1300] LFt[6][2:10:1300] "w p lt rgb 'black' pt 7 ps 0.75 t ''";
     @gp :- aassetst[7,2:10:1300] LFt[7][2:10:1300] "w p lt rgb 'black' pt 7 ps 0.75 t ''";
+    @gp :- "set auto fix";
 end
 
 # provides the second part of Figure 4
