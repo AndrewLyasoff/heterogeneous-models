f5ff1b089c944b3bb95e27ff807cbe7b558acb34
Andrew Lyasoff <andrew.lyasoff@gmail.com>
2023-07-19 22:36:15 -0400

    Add files via upload

---
+++ SATHA-Sec-3/ini-setup-RMT-ch18.jl
@@ -0,0 +1,42 @@
+######################################################################################################
+#
+# Julia code
+#
+# Sets the parameter values from Sec. 18.7 in "Recursive Macroeconomic Theory" (RMT)
+#                                     by Lars Ljungqvist and Thomas Sargent
+#
+# This code supplements the paper "Self-Aware Transport of Heterogeneous Agents in Incomplete Markets" [SATHA]
+#                                        by Andrew Lyasoff  (www.andrewlyasoff.tech)
+# 
+# Copyright © 2019-2023 Andrew Lyasoff <alyasoff@bu.edu>
+# SPDX-License-Identifier: Apache-2.0
+#
+###################################################################################################
+
+#=
+This code assigns values to:
+
+CPROB, nos, BSP, hours, wage, ι, ρ, β, R
+=#
+
+#=
+Thransition probability matrix for the idiosyncratic state
+=#
+CPROB=[0.0262397497796233 0.152923483594817 0.361483063911416 0.328567584707170 0.114741751017859 0.0152660804766739 0.000778286512441828; 0.0160443669891157 0.114741751017859 0.328567584707170 0.361483063911415 0.152923483594817 0.0247005562212874 0.00153919355833587; 0.00945177150556068 0.0828345049143809 0.287445156270025 0.382789297365280 0.196113758787202 0.0384369616149092 0.00292854954264210; 0.00536221880153007 0.0575309935179400 0.242023809517260 0.390165956326541 0.242023809517260 0.0575309935179400 0.00536221880153009; 0.00292854954264206 0.0384369616149092 0.196113758787202 0.382789297365280 0.287445156270025 0.0828345049143808 0.00945177150556065; 0.00153919355833585 0.0247005562212875 0.152923483594817 0.361483063911416 0.328567584707170 0.114741751017859 0.0160443669891157; 0.000778286512441806 0.0152660804766738 0.114741751017859 0.328567584707170 0.361483063911416 0.152923483594817 0.0262397497796233];
+
+nos=size(CPROB)[1]; # total number of states
+[sum(CPROB[i,:]) for i=1:nos];
+
+mM=CPROB'-I;
+mM[end,:]=[1 for i=1:nos];
+rhs=[floor(i/nos) for i=1:nos];
+BSP=mM\rhs; #the list of steady-state probabilities
+
+hours = exp.([-1.20000000000000, -0.800000000000000, -0.400000000000000, 0, 0.400000000000000, 0.800000000000000, 1.20000000000000]);
+wage=0.2;
+inc=wage*hours;
+ι=inc'*BSP; # payoff from the bond (= the aggregate output in the economy)
+
+β=0.96; # impatience discount factor 
+R=3.0; # risk aversion
+
---
+++ SATHA-Sec-3/run-SATHA-0.jl
@@ -0,0 +1,71 @@
+######################################################################################################
+#
+# Julia code
+#
+# Executes the method described in Sec. 3 of the paper
+#       "Self-Aware Transport of Heterogeneous Agents in Incomplete Markets" [SATHA] by Andrew Lyasoff
+#
+# The code provides a verifiable solution to the example from
+#                Sec. 18.7 in "Recursive Macroeconomic Theory" (RMT) by Lars Ljungqvist and Thomas Sargent 
+#
+# This code supplements the paper "Self-Aware Transport of Heterogeneous Agents in Incomplete Markets" [SATHA]
+#                                        by Andrew Lyasoff (www.andrewlyasoff.com)
+#
+# Copyright © 2019-2023 Andrew Lyasoff <alyasoff@bu.edu>
+# SPDX-License-Identifier: Apache-2.0
+#
+###################################################################################################
+
+
+begin
+    using Serialization, FileIO
+    using LinearAlgebra
+    using Interpolations
+    using Roots
+    using Gnuplot
+    using Optim
+    include("functions-SATHA-0.jl");
+    include("ini-setup-RMT-ch18.jl");
+end;
+
+### INITIALIZATION
+
+begin
+    ρ=-log(β)
+    # initial guess for the spot price
+    SPOT=ι; # = face value = total income in the economy
+    # initial guess for portfolio functions
+    θ=[x->40.0*x-8.0 for k=1:nos];
+    H=[x->x+(40.0*x-8.0)*SPOT for k=1:nos];
+    # accordingly, initial choice for the inverse mapping in (3.3) 
+    invH=[y->(y+8.0*SPOT)/(1+40.0*SPOT) for k=1:nos];
+    # upper bound on the grid over consumption [Step 1 in (3.5)]
+    c_high=FindUpperC(nos,SPOT,invH,CPROB,inc,ι,R,β);
+    gsz=150 #The only place to set the number of grid points in the consumption range,
+    g_step=(c_high/gsz);
+end;
+
+
+### THE MAIN ROUTINE IS NEXT
+
+@time no_iter, conv_check, conv_check_c, clearing, upper_bb, F, Fval, θ, invH, SPOT, cnext, invcnext, g_step, gsz, sol, accu0, accu, ANSATZ, CLR=find_equil(nos,0,235,0.00001,g_step,gsz,θ,invH,SPOT,CPROB,inc,BSP,ι,R,549,299,199,β);
+# 8114.527882 seconds (182.17 G allocations: 8.445 TiB, 12.72% gc time, 0.08% compilation time)
+
+
+#check the convergence
+no_iter, conv_check, conv_check_c, clearing, accu0, accu
+#=
+returns: (235, 9.084474162968093e-5, 2.1632642125402057e-6, -1.7387766322006504e-6, 5.545564008002657e-14, 3.1645982615513546e-5)
+=#
+
+#### save the results produced by the main routine
+
+#
+# create the list of data to save
+# N.B. spline objects cannot be saved and must be restored later from the respective abscissas and values
+#
+
+begin
+    saved_vals=[CPROB, BSP, hours, wage, inc, ι, ρ, R, no_iter, conv_check, conv_check_c, clearing, g_step, gsz, upper_bb, Fval, sol, accu0, accu, ANSATZ, CLR, SPOT];
+    serialize("output-SATHA-0.jls", saved_vals); #dump to a file
+end;
